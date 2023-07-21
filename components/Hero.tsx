53110af6a417c70bccb88f4e1d9bd8de4d44be00
Muhammad Bilal <bilalofficialmail123@gmail.com>
2023-07-21 15:36:22 +0500

    stopped work and moved to next basic project , promptopia by js mastery

---
+++ components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <div className="hero__title">
+          Find, book or rent a CAR — quickly and easily
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
